f71d5f3e96824341087cebc9eb57ec562881316a
Charlie Hsieh <charlieofficial@gmail.com>
2018-03-13 22:05:03 +0800

    typo

--- lib/app.sh
+++ lib/app.sh
@@ -6,7 +6,6 @@
 #------------------------------------
 # Include libaries
 #------------------------------------
-echo $LIB
 LIB_SCRIPTS="$(ls $LIB |grep -E "^L_[0-9][0-9]_[^[:space:]]+(.sh)$" | sort -n)"
 for LIB_SCRIPT in $LIB_SCRIPTS
 do
