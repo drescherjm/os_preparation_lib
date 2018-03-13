d5b7c088c57b03411126d28997d089dc59d90ae3
Charlie Hsieh <charlieofficial@gmail.com>
2018-03-13 21:43:57 +0800

    move all lib to here, from os_preparation , os_security

---
+++ README.md
@@ -0,0 +1,4 @@
+# CentOS Linux Server OS Preparation Lib
+This is used by os_preparation.
+Please reference:
+https://github.com/charlietag/os_preparation.git
---
+++ lib/app.sh
@@ -0,0 +1,15 @@
+#------------------------------------
+# Define lib path
+#------------------------------------
+LIB="${OS_PREPARAION_LIB}/lib"
+
+#------------------------------------
+# Include libaries
+#------------------------------------
+LIB_SCRIPTS="$(ls $LIB |grep -E "^L_[0-9][0-9]_[^[:space:]]+(.sh)$" | sort -n)"
+for LIB_SCRIPT in $LIB_SCRIPTS
+do
+  . $LIB/$LIB_SCRIPT
+  #echo "$LIB_SCRIPT"
+done
+#exit
