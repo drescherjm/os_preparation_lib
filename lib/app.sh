f98329e53ce162fce776189ca7be0ab62c6ba200
Charlie Hsieh <charlieofficial@gmail.com>
2018-03-13 22:04:31 +0800

    move var LIB to start.sh

--- lib/app.sh
+++ lib/app.sh
@@ -1,11 +1,12 @@
 #------------------------------------
 # Define lib path
 #------------------------------------
-LIB="${OS_PREPARAION_LIB}/lib"
+#LIB="${OS_PREPARATION_LIB}/lib"   # This var move to start.sh
 
 #------------------------------------
 # Include libaries
 #------------------------------------
+echo $LIB
 LIB_SCRIPTS="$(ls $LIB |grep -E "^L_[0-9][0-9]_[^[:space:]]+(.sh)$" | sort -n)"
 for LIB_SCRIPT in $LIB_SCRIPTS
 do
