cb357c65adaf514c16730af937d11aaa2c7c2e61
Charlie Hsieh <charlieofficial@gmail.com>
2019-08-12 04:22:22 +0800

    add more check for more senareo

--- lib/L_07_safe_delete.sh
+++ lib/L_07_safe_delete.sh
@@ -1,52 +1,183 @@
 #-------------------------------------------------
 # Function
 #-------------------------------------------------
-#CHECK_IF_VAR_EXISTS_IN_REGEX() {
-#}
+CHECK_IF_VAR1_IN_VAR2() {
+  local keywords_var1="$1"
+  local keywords_var2="$2"
+  # Bash array slice ref. https://unix.stackexchange.com/questions/82060/bash-slice-of-positional-parameters
+  #local keywords_var2="${@:2}"
 
-CHECK_IF_VAR_EXISTS_IN() {
-  local check_keyword="$1"
-  local check_from_keywords="$2"
   local check_return='false'
-  for check_from_keyword in ${check_from_keywords[@]}; do
-    if [[ "${check_keyword}" == "${check_from_keyword}" ]]; then
-      check_return='true'
-    fi
+  local match_words=""
+
+  # --For Debug--
+  #echo "var1: ${keywords_var1[@]}"
+  #echo "var2: ${keywords_var2[@]}"
+
+  # ----------------------------------------------------
+  # Ref. https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+  # Disable filename expansion (globbing)
+  # set -f    # Disable
+  # echo -n "(check)"; set -o | grep noglob
+  # set +f    # Enbale
+  # echo -n "(check)"; set -o | grep noglob
+  # ----------------------------------------------------
+  set -f
+  for keyword_var2 in ${keywords_var2[@]}; do
+    for keyword_var1 in ${keywords_var1[@]}; do
+      if [[ "${keyword_var1}" == "${keyword_var2}" ]]; then
+        #check_return='true'
+        match_words="${match_words} ${keyword_var1}"
+      fi
+    done
   done
-  echo "${check_return}"
+  set +f
+
+  #echo "${check_return}|${match_words}"
+  echo "${match_words}"
 }
 
-SAFE_DELETE () {
-  local delete_files="$(readlink -m $1)"
-  local skip_files="$((find / -maxdepth 1 ;  readlink -m /* )|sort -n |uniq)"
-  local if_error_exists='false'
+CHECK_IF_VAR1_IN_VAR2_GREP() {
+  local keywords_var1="$1"
+  local keywords_var2="$2"
 
-  # --- check of exists in skip_files ---
-  for delete_file in ${delete_files[@]}; do
-    local if_exists="$(CHECK_IF_VAR_EXISTS_IN "${delete_file}" "${skip_files}")"
-    if [[ "${if_exists}" == 'true' ]]; then
-      echo "FATAL ERROR: deleting ${delete_file} is NOT allowed..."
-      if_error_exists='true'
-    fi
+  local check_return='false'
+  local match_words=""
+
+  # --For Debug--
+  #echo "var1: ${keywords_var1[@]}"
+  #echo "var2: ${keywords_var2[@]}"
+
+  set -f
+  for keyword_var2 in ${keywords_var2[@]}; do
+    for keyword_var1 in ${keywords_var1[@]}; do
+      # Ref. https://stackoverflow.com/questions/18191548/how-to-grep-asterisk-without-escaping
+      # Use the flag -F to search for fixed strings -- instead of regular expressions
+      # Works with : set -f
+      #echo "--- ${keyword_var1} v.s. ${keyword_var2} ---"
+      #echo "${keyword_var1}" | grep -F "${keyword_var2}"
+
+      match_words="${match_words} $(echo "${keyword_var1}" | grep -F "${keyword_var2}")"
+    done
   done
+  match_words="$(echo "${match_words}"| xargs -n 1 | sort -n | uniq | xargs)"
+  set +f
 
-  # --- check if exists in skip_files ---
-  if [[ "${if_error_exists}" == 'true' ]]; then
-    echo "[FATAL ERROR] founds ... exit process..."
+  echo "${match_words}"
+
+}
+
+# --- Check exactly the same name and check for dangerouse keywords ---
+# Avoid user deletes files using wrong wildcard
+CHECK_FILE_KEYWORD_DANGER() {
+  local keywords_var1="$1"
+  local keywords_var2='
+    .*
+    *.*
+  '
+
+  local match_words=''
+  # --- check files ---
+  # --For Debug--
+  #CHECK_IF_VAR1_IN_VAR2_GREP "${keywords_var1}" "${keywords_var2}"
+  #exit
+
+  local match_words="$(CHECK_IF_VAR1_IN_VAR2_GREP "${keywords_var1}" "${keywords_var2}")"
+  if [[ -n "${match_words}" ]]; then
+    echo "(keyword danger check) FATAL ERROR: deleting \"${match_words}\" is NOT allowed..."
+
     exit 1
   fi
 
-  # --- start to delete safe files ---
+}
+
+CHECK_FILE_KEYWORD() {
+  local keywords_var1="$1"
+  local keywords_var2='
+    .
+    ..
+    *
+    /
+    .*
+    *.*
+  '
+  #Use with array slice above, but not so general. So still pass two string as argument to function
+  #local keywords_var2=(
+  #  '.'
+  #  '..'
+  #  '.*'
+  #  '*'
+  #  '*.*'
+  #  '/'
+  #)
+  local match_words=''
+
+  # --- check files ---
+  # --For Debug--
+  #CHECK_IF_VAR1_IN_VAR2 "${keywords_var1}" "${keywords_var2[@]}"
+  #exit
+  #local match_words="$(CHECK_IF_VAR1_IN_VAR2 "${keywords_var1}" "${keywords_var2[@]}")"
+
+  local match_words="$(CHECK_IF_VAR1_IN_VAR2 "${keywords_var1}" "${keywords_var2}")"
+  if [[ -n "${match_words}" ]]; then
+    echo "(keyword check) FATAL ERROR: deleting \"${match_words}\" is NOT allowed..."
+
+    exit 1
+  fi
+
+}
+
+# --- Check via readlink -m keyword_files ---
+# Avoid user deletes folders under /
+CHECK_FILE_READLINK() {
+  local keywords_var1="$(readlink -m $1)"
+  local keywords_var2="$((find / -maxdepth 1 ;  readlink -m /* )|sort -n |uniq)" # basicly system files here are dangerous files
+  local match_words=''
+
+  # --- check files ---
+  local match_words="$(CHECK_IF_VAR1_IN_VAR2 "${keywords_var1}" "${keywords_var2}")"
+  if [[ -n "${match_words}" ]]; then
+    echo "(folder check) FATAL ERROR: deleting \"${match_words}\" is NOT allowed..."
+
+    exit 1
+  fi
+}
+
+SAFE_DELETE () {
+  local keywords_var1="$1"
+
+  # --- check files ---
+  CHECK_FILE_KEYWORD_DANGER "${keywords_var1}"
+  CHECK_FILE_KEYWORD "${keywords_var1}"
+  CHECK_FILE_READLINK "${keywords_var1}"
+
+
+  local delete_files="$(readlink -m $1)"
+  #echo "------ Deleting Files(debug) -----"
+  #echo "${delete_files}"
+  #echo "------ Deleting Files(debug) -----"
+  #echo 
+
+
+  # --- start deleting files ---
+  echo "------ Start Deleting Files -----"
   for delete_file in ${delete_files[@]}; do
     echo "Deleting ${delete_file} ..."
     rm -fr ${delete_file}
   done
+  echo "------ End Deleting Files -----"
 }
 
 #-------------------------------------------------
-# Usage:
-# SAFE_DELETE "{FILE|FOLER}"
+# TEST Sample Code (Usage)
 #-------------------------------------------------
-# --- TEST Sample Code ---
-# DELETE_FROM="/root/delete_me"
-# SAFE_DELETE "${DELETE_FROM}"
+# --- Should be deleting /root/* (be careful) ---
+#DELETE_FILE="/root/*"
+
+
+# --- Should be failed ---
+#DELETE_FILE="/root/delete_me/.*"
+#DELETE_FILE="/root/"
+
+# --- safe delete command usage ---
+#SAFE_DELETE "${DELETE_FILE}"
