a4a729078b13b1b4571d82744a879fd6bf473549
Charlie Hsieh <charlieofficial@gmail.com>
2020-04-12 15:27:38 +0800

    change to support CentOS 8

--- lib/L_00_yum.sh
+++ lib/L_00_yum.sh
@@ -1,4 +1,10 @@
 #-----------------------------------------------------------------------------------------
-# Before everything clean all yum cache
+# Before everything clean all yum / dnf cache
 #-----------------------------------------------------------------------------------------
-`which yum` clean all >/dev/null 2>/dev/null
+OS_RELEASE_VER="$(cat /etc/centos-release |grep 'release 8')"
+
+if [[ -n "${OS_RELEASE_VER}" ]]; then
+  dnf clean all >/dev/null 2>/dev/null
+else
+  yum clean all >/dev/null 2>/dev/null
+fi
