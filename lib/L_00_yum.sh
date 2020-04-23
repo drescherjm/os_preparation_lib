73fee81aad028612d5484775942d4da8f6cbc67b
Charlie Hsieh <charlieofficial@gmail.com>
2020-04-23 16:22:15 +0800

    fix typo

--- lib/L_00_yum.sh
+++ lib/L_00_yum.sh
@@ -4,35 +4,35 @@
 OS_RELEASE_VER="$(cat /etc/centos-release |grep 'release 8')"
 
 if [[ -n "${OS_RELEASE_VER}" ]]; then
-  EXEC_CMD="dnf"
+  REPO_EXEC_CMD="dnf"
 else
-  EXEC_CMD="yum"
+  REPO_EXEC_CMD="yum"
 fi
 
 
-$EXEC_CMD clean all >/dev/null 2>/dev/null
+$REPO_EXEC_CMD clean all >/dev/null 2>/dev/null
 
 ############### Fetch dnf repo retry Loop (For epel-modular) #############
 #let dnf_repo_install_retry++
 #for ((i=1; i<=dnf_repo_install_retry; i++)); do
-echo "Updating ${EXEC_CMD} Repo list....."
+echo "Updating ${REPO_EXEC_CMD} Repo list....."
 
 for ((i=1; ; i++)); do
 
   # ---------- Check DNF Repo Installation -----------
-  local dnf_repo_check="$($EXEC_CMD repolist >/dev/null 2>/dev/null && echo "Success")"
-  if [[ -n "${dnf_repo_check}" ]]; then
-    echo "${EXEC_CMD} Repo is updated successfully!"
+  DNF_REPO_CHECK="$($REPO_EXEC_CMD repolist >/dev/null 2>/dev/null && echo "Success")"
+  if [[ -n "${DNF_REPO_CHECK}" ]]; then
+    echo "${REPO_EXEC_CMD} Repo is updated successfully!"
     break
   fi
 
-  if [[ -z "${dnf_repo_check}" ]]; then
-    echo "${EXEC_CMD} Repo is not updated yet!"
+  if [[ -z "${DNF_REPO_CHECK}" ]]; then
+    echo "${REPO_EXEC_CMD} Repo is not updated yet!"
     #[[ $i -eq $dnf_repo_install_retry ]] && exit
     [[ $i -gt $dnf_repo_install_retry ]] && exit
   fi
 
-  echo -n "${EXEC_CMD} Repo updating (try: $i) "
+  echo -n "${REPO_EXEC_CMD} Repo updating (try: $i) "
   #sleep 1; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n "."; echo ""
   sleep 1; echo -n "."; echo ""
 done
