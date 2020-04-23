8870f96f188f90ec46e661564a637ba9783ffccf
Charlie Hsieh <charlieofficial@gmail.com>
2020-04-23 17:33:04 +0800

    do yum repo update only for CentOS8 (because epel-modular usually failed while updating)

--- lib/L_00_yum.sh
+++ lib/L_00_yum.sh
@@ -13,33 +13,35 @@ L_UPDATE_REPO() {
 
   $repo_exec_cmd clean all >/dev/null 2>/dev/null
 
-  ############### Fetch dnf repo retry Loop (For epel-modular) #############
-  local dnf_repo_install_retry=5000
+  if [[ -n "${os_release_ver}" ]]; then
+    ############### Fetch dnf repo retry Loop (For epel-modular) #############
+    local dnf_repo_install_retry=5000
 
-  #let dnf_repo_install_retry++
-  #for ((i=1; i<=dnf_repo_install_retry; i++)); do
-  echo "Updating ${repo_exec_cmd} Repo list....."
+    #let dnf_repo_install_retry++
+    #for ((i=1; i<=dnf_repo_install_retry; i++)); do
+    echo "Updating ${repo_exec_cmd} Repo list....."
 
-  for ((i=1; ; i++)); do
+    for ((i=1; ; i++)); do
 
-    # ---------- Check DNF Repo Installation -----------
-    local dnf_repo_check="$($repo_exec_cmd repolist >/dev/null 2>/dev/null && echo "Success")"
-    if [[ -n "${dnf_repo_check}" ]]; then
-      echo "${repo_exec_cmd} Repo is updated successfully!"
-      break
-    fi
+      # ---------- Check DNF Repo Installation -----------
+      local dnf_repo_check="$($repo_exec_cmd repolist >/dev/null 2>/dev/null && echo "Success")"
+      if [[ -n "${dnf_repo_check}" ]]; then
+        echo "${repo_exec_cmd} Repo is updated successfully!"
+        break
+      fi
 
-    if [[ -z "${dnf_repo_check}" ]]; then
-      echo "${repo_exec_cmd} Repo is not updated yet!"
-      [[ $i -gt $dnf_repo_install_retry ]] && exit
-    fi
+      if [[ -z "${dnf_repo_check}" ]]; then
+        echo "${repo_exec_cmd} Repo is not updated yet!"
+        [[ $i -gt $dnf_repo_install_retry ]] && exit
+      fi
 
-    echo -n "${repo_exec_cmd} Repo updating (try: $i) "
-    #sleep 1; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n "."; echo ""
-    sleep 1; echo -n "."; echo ""
-  done
+      echo -n "${repo_exec_cmd} Repo updating (try: $i) "
+      #sleep 1; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n "."; echo ""
+      sleep 1; echo -n "."; echo ""
+    done
 
-  echo ""
-  ############### Fetch dnf repo retry Loop (For epel-modular) #############
+    echo ""
+    ############### Fetch dnf repo retry Loop (For epel-modular) #############
+  fi
 }
 
