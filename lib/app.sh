4eca857dd96a89999d603b9505f6e792117b2023
Charlie Hsieh <charlieofficial@gmail.com>
2020-06-18 16:36:05 +0800

    show actions messages

--- lib/app.sh
+++ lib/app.sh
@@ -21,13 +21,22 @@ LIB="${OS_PRE_LIB}/lib"
 #-----------------------------------------------------------------------------------------
 # NTP update date time and hwclock to prevent mariadb cause systemd warning
 #-----------------------------------------------------------------------------------------
+echo "---------------------------------------------------"
+echo "NTP(chrony) ---> pool.ntp.org"
+echo "---------------------------------------------------"
 rpm --quiet -q chrony || $REPO_EXEC_CMD install -y chrony
 # make sure chronyd stop first , before syncing time using chronyd command!
 systemctl stop chronyd
 systemctl disable chronyd
+
+echo "RUN: chronyd -q 'pool pool.ntp.org iburst'"
 chronyd -q 'pool pool.ntp.org iburst'
 
+echo "RUN: hwclock -w"
 hwclock -w
+
+echo "---------------------------------------------------"
+echo ""
 
 #------------------------------------
 # Include libaries
